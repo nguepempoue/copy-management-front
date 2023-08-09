f19c3e0b98ac9f55bfc30e34d15cc8abc9443709
nguepempoue <nguepempouejulio@gmail.com>
2023-08-09 05:13:24 +0200

    ébut intégration du tempale

---
+++ src/app/app-modules/app-modules.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppModulesRoutingModule } from './app-modules-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppModulesRoutingModule
+  ]
+})
+export class AppModulesModule { }
